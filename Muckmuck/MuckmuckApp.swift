f02a422e54d160444f20f6dcf0771ffb6519a8d0
0tak2 <0tak2.code@gmail.com>
2025-03-22 09:48:51 +0900

    Initial Commit

---
+++ Muckmuck/MuckmuckApp.swift
@@ -0,0 +1,17 @@
+//
+//  MuckmuckApp.swift
+//  Muckmuck
+//
+//  Created by 임영택 on 3/22/25.
+//
+
+import SwiftUI
+
+@main
+struct MuckmuckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MuckmuckTests/MuckmuckTests.swift
@@ -0,0 +1,17 @@
+//
+//  MuckmuckTests.swift
+//  MuckmuckTests
+//
+//  Created by 임영택 on 3/22/25.
+//
+
+import Testing
+@testable import Muckmuck
+
+struct MuckmuckTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
